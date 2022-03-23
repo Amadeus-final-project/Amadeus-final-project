456ba923f6af1edf85af601a6839ecd095c59951
istefanoff <official.stefanov@gmail.com>
2022-03-23 20:04:36 +0200

    Added validations and LoginDTO

---
+++ PDS/src/main/java/com/example/pds/model/user/LoginDTO.java
@@ -0,0 +1,19 @@
+package com.example.pds.model.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginDTO {
+
+    @NotNull(message = "Please enter username")
+    private String username;
+
+    @NotNull(message = "Please enter password")
+    private String password;
+}
--- PDS/src/main/java/com/example/pds/model/user/RegisterDTO.java
+++ PDS/src/main/java/com/example/pds/model/user/RegisterDTO.java
@@ -3,7 +3,10 @@ package com.example.pds.model.user;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 @Setter
@@ -11,11 +14,24 @@ import javax.validation.constraints.Pattern;
 @NoArgsConstructor
 public class RegisterDTO {
     private int id;
+
+    @Length(min = 5, message = "Username should be at least 5 symbols")
     private String username;
+
+    @NotBlank(message = "First name is mandatory")
+    @Pattern(regexp = "^[A-Za-zА-Яа-я]+", message = "First name should be only letters")
     private String firstName;
+
+    @NotBlank(message = "Last name is mandatory")
+    @Pattern(regexp = "^[A-Za-zА-Яа-я]+", message = "Last name should be only letters")
     private String lastName;
+
+    @Length(min = 8, message = "Password must be at least 8 symbols")
     private String password;
+
     private String confirmPassword;
+
+    @Email(message = "Invalid email address")
     private String email;
 
 }
