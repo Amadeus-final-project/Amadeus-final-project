2e58d6063342f4e838276c0b53ee6267d6449227
Mihroni <mihmilev@gmail.com>
2022-04-13 16:29:10 +0300

    Added Stripe payment

---
+++ PDS/src/main/java/com/example/pds/controllers/paymentControllers/ChargeController.java
@@ -0,0 +1,37 @@
+package com.example.pds.controllers.paymentControllers;
+
+import com.example.pds.model.payments.ChargeRequest;
+import com.example.pds.model.payments.StripeService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChargeController {
+    @Autowired
+    private StripeService paymentsService;
+    private Object Currency;
+
+    @PostMapping("/charge")
+    public String charge(ChargeRequest chargeRequest, Model model)
+            throws StripeException {
+        chargeRequest.setDescription("Example charge");
+        chargeRequest.setCurrency(ChargeRequest.Currency.EUR);
+        Charge charge = paymentsService.charge(chargeRequest);
+        model.addAttribute("id", charge.getId());
+        model.addAttribute("status", charge.getStatus());
+        model.addAttribute("chargeId", charge.getId());
+        model.addAttribute("balance_transaction", charge.getBalanceTransaction());
+        return "result";
+    }
+
+    @ExceptionHandler(StripeException.class)
+    public String handleError(Model model, StripeException ex) {
+        model.addAttribute("error", ex.getMessage());
+        return "result";
+    }
+}
